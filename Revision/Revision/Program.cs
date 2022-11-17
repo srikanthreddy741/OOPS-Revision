ae56a9c2d40c98a0c9e5986de0ce420c451c895f
Srikanth Reddy <bandarisrikanth741@gmail.com>
2022-11-17 19:22:11 +0530

    added inhertence method

---
+++ Revision/Revision/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//[Inheritance]
+
+using System;
+
+public class Animal
+{
+    public void eat()
+    {
+        Console.WriteLine("Eating");
+    }
+}
+
+public class Dog : Animal
+{
+    public void bark()
+    {
+        Console.WriteLine("Barking");
+    }
+}
+
+public class inheritence
+{
+    public static void Main(string[] args)
+    {
+        Dog d1 = new Dog();
+        d1.eat();
+        d1.bark();
+    }
+}
