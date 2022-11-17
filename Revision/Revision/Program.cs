f1c10ff6a7e789a84b1007df010e13294993452a
Srikanth Reddy <bandarisrikanth741@gmail.com>
2022-11-17 19:23:41 +0530

    added methodoverload method

--- Revision/Revision/Program.cs
+++ Revision/Revision/Program.cs
@@ -1,30 +1,54 @@
 ﻿
-//[Inheritance]
+////[Inheritance]
+
+//using System;
+
+//public class Animal
+//{
+//    public void eat()
+//    {
+//        Console.WriteLine("Eating");
+//    }
+//}
+
+//public class Dog : Animal
+//{
+//    public void bark()
+//    {
+//        Console.WriteLine("Barking");
+//    }
+//}
+
+//public class inheritence
+//{
+//    public static void Main(string[] args)
+//    {
+//        Dog d1 = new Dog();
+//        d1.eat();
+//        d1.bark();
+//    }
+//}
 
 using System;
 
-public class Animal
+
+public class call
 {
-    public void eat()
+    public static int add(int a, int b)
     {
-        Console.WriteLine("Eating");
+        return a + b;
+    }
+    public static int add(int a, int b, int c)
+    {
+        return a + b + c;
     }
 }
 
-public class Dog : Animal
+public class methodoverloading
 {
-    public void bark()
+    public static void Main()
     {
-        Console.WriteLine("Barking");
-    }
-}
-
-public class inheritence
-{
-    public static void Main(string[] args)
-    {
-        Dog d1 = new Dog();
-        d1.eat();
-        d1.bark();
+        Console.WriteLine(call.add(20, 48));
+        Console.WriteLine(call.add(15, 43, 65));
     }
 }
